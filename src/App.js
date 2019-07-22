1194dc33861094d448b21578208be94cddbf9481
Aubrey <tsambataret@gmail.com>
2019-07-22 15:46:10 +0800

    cart added, need to add functionality for de and increment and remove item from cart

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { Component } from "react";
+import { Switch, Route } from "react-router-dom";
 import logo from "./logo.svg";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -6,19 +7,25 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./components/Navbar";
 import Productlist from "./components/Productlist";
 import Details from "./components/Details";
-import Cart from "./components/Cart";
+import Cart from "./components/Cart/Cart"; 
 import Default from "./components/Default";
+import Modal from './components/Modal'
 
-function App() {
-  return (
-    <React.Fragment>
-      <Navbar> </Navbar>
-      <Productlist></Productlist>
-      <Details></Details>
-      <Cart></Cart>
-      <Default></Default>
-    </React.Fragment>
-  );
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Productlist} />
+          <Route path="/details" component={Details} />
+          <Route path="/cart" component={Cart} />
+          <Route component={Default} />
+        </Switch>
+        <Modal/>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
